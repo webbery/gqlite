be01282bcd61857b790c0d912f7af3066306e3d6
v_yuanwenbin <v_yuanwenbin@baidu.com>
2022-07-19 16:45:26 +0800

    ci: add memleak check; fix: update event emitter implement

--- include/base/system/EventEmitter.h
+++ include/base/system/EventEmitter.h
@@ -7,13 +7,28 @@
 class GEventEmitter {
 public:
   GEventEmitter();
+  ~GEventEmitter();
 
   void emit(int, std::any& args);
 
   void on(int, std::function<void(const std::any&)> f);
 
   void clear();
+
+private:
+  class Job {
+  public:
+    Job(std::function<void(const std::any&)> f, const std::any& args):_args(args), _f(f) {}
+    void operator()() {
+      _f(_args);
+    }
+
+  private:
+    std::function<void(const std::any&)> _f;
+    std::any _args;
+  };
 private:
   std::map<int, std::function<void(const std::any&)>> _listeners;
-  parlay::scheduler<std::function<void()>> _scheduler;
+  parlay::scheduler<Job>* _scheduler;
+  std::vector<Job*> _jobs;
 };
--- src/base/system/EventEmitter.cpp
+++ src/base/system/EventEmitter.cpp
@@ -1,16 +1,21 @@
 #include "base/system/EventEmitter.h"
 
 GEventEmitter::GEventEmitter()
+:_scheduler(new parlay::scheduler<Job>)
 {}
+
+GEventEmitter::~GEventEmitter() {
+  delete _scheduler;
+  for (Job* job: _jobs) {
+    delete job;
+  }
+}
 
 void GEventEmitter::emit(int event, std::any& args) {
   if (_listeners.count(event)) {
-    std::function<void()> lambda = [&job = this->_listeners[event], args = args]() {
-      job(args);
-    };
-    // _scheduler.spawn(lambda);
-    // _listeners[event](args);
-    lambda();
+    Job* job = new Job(_listeners[event], args);
+    _jobs.push_back(job);
+    _scheduler->spawn(job);
   }
 }
 
