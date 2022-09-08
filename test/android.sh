86a82de07fd1bc8a617db7cc0b6c39b4e654611c
yuanwenbin <yaunwenbin@kuandeng.com>
2022-09-08 19:42:04 +0800

    run script

--- test/android.sh
+++ test/android.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
-adb push ../build/test/vnet /sdcard/vnet
+adb push build/test/vnet /sdcard/vnet
+adb shell sh /sdcard/vnet
+adb push build/test/storage /sdcard/test/storage
+adb push build/test/movielens /sdcard/movielens
+adb push build/test/regress /sdcard/regress
+adb push build/test/query /sdcard/query
+adb push build/test/grammar /sdcard/grammar
